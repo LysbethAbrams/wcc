1452de5adbfdf56d0e29cea46b62070043df171f
LysbethAbrams <lysabrams@gmail.com>
2016-11-12 19:50:24 -0500

    Add files via upload

---
+++ ariel.py
@@ -0,0 +1,7 @@
+age = 30
+first_name = 'Ariel'
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+print(first_name + ' is ' + str(age) + ' years old now, but she will turn ')
+print( str(next_age) + ' in ' + birth_month + '. Her birth stone is ' + birth_stone +'.')
