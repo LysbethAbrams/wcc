199796f7632b40e35d93361d98efbb0564bb92c2
LysbethAbrams <lysabrams@gmail.com>
2016-11-13 20:26:36 -0500

    Add files via upload

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,6 +1,6 @@
 meal_price = float(raw_input('How much was your meal? '))
 tip = meal_price * .20
 total_price = meal_price + tip
-print('You should tip $' + str(tip))
+
 print('You should tip $' + str(tip))
 print('Your total cost would be $' + str(total_price))
