0dd7fdfc39b6acf12fa11edd23e943989a36603b
LysbethAbrams <lysabrams@gmail.com>
2016-11-13 19:30:17 -0500

    Add files via upload

--- input.py
+++ input.py
@@ -6,5 +6,5 @@ name = raw_input('What is your name?')
 age = raw_input('How old are you?')
 print(name + ' is ' + age + ' years old.')
 age = raw_input('How old are you?')
-dog_years = age * 7
-print('You are ' + dog_years + 'old in dog years.')
+dog_years = int(age) * 7
+print('You are ' + str(dog_years) + ' years old in dog years.')
