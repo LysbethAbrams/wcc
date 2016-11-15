4239f056658bdd8bc7e2cd5390f3d338caeef253
LysbethAbrams <lysabrams@gmail.com>
2016-11-14 21:06:59 -0500

    Add files via upload

---
+++ boolean_inclusive.py
@@ -0,0 +1,6 @@
+age = int(raw_input('How old are you?'))
+print( age >= 5 and age <= 10)
+
+greater_than_equal_to_five = age >= 5
+less_than_equal_to_ten = age <= 10
+print( greater_than_equal_to_five and less_than_equal_to_ten )
---
+++ boolean_not_inclusive.py
@@ -0,0 +1,6 @@
+age = int(raw_input('How old are you?'))
+print( age > 5 and age < 10)
+
+bigger_than_five = age > 5
+less_than_ten = age < 10
+print( bigger_than_five and less_than_ten )
